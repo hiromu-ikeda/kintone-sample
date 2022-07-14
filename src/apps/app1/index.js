939715b1e4fb87ee9f1e74e28aa95fc49ef10aec
hiromu-ikeda <hiromu.ikeda@ynot.jp>
2022-07-14 23:36:47 +0900

    7/14

--- src/apps/app1/index.js
+++ src/apps/app1/index.js
@@ -3,7 +3,7 @@ const events = ['app.record.create.show', 'app.record.edit.show'];
 kintone.events.on(events, event => {
   const el = kintone.app.record.getHeaderMenuSpaceElement();
   const hello = 'hello';
-  const world = 'world!';
+  const world = 'world';
   const message = {
     hello,
     world
